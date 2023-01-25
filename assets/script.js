6027a366d15864e443a02b2fe3d7c79be3c56c6a
collin-mccarter <122714302+collin-mccarter@users.noreply.github.com>
2023-01-24 22:03:53 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics() {
 for (var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
-}
+  }
+ }
 
-/*
+function selectTopic() {
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,5 +18,10 @@ if (topic === 'HTML') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
+ }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
